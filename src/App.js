9ebf9a9839640b8529b9ff7441d666f80e3912ee
AhmedElaguab <elaguabahmed97@gmail.com>
2019-06-20 14:14:45 +0100

    feat: add the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <h2>Start the app.</h2>
+      <NavBar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
