82d9fbc37667bee933ef2c4e4be318ffe8e7a4d1
AhmedElaguab <elaguabahmed97@gmail.com>
2019-06-25 19:02:04 +0100

    feat: copy code on clicking

--- src/components/GradientPage.jsx
+++ src/components/GradientPage.jsx
@@ -43,13 +43,43 @@ function GradientPage(props) {
               }}
             >
               {gradient.name}
-              <CodeIcon className={classes.codeIcon} />
+              <CodeIcon
+                className={classes.codeIcon}
+                onClick={() => copyCode(gradient)}
+              />
             </GradientPageInner>
           )}
         </Grid>
       </Grid>
     </Container>
   )
+}
+
+function copyCode(gradient) {
+  const rules = `
+  background: ${gradient.colors[0]};  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, ${gradient.colors[0]}, ${
+    gradient.colors[1]
+  });  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, ${gradient.colors[0]}, ${
+    gradient.colors[1]
+  }); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  `
+
+  // Create new element
+  var el = document.createElement("textarea")
+  // Set value (string to be copied)
+  el.value = rules
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute("readonly", "")
+  el.style = { position: "absolute", left: "-9999px" }
+  document.body.appendChild(el)
+  // Select text inside element
+  el.select()
+  // Copy text to clipboard
+  document.execCommand("copy")
+  // Remove temporary element
+  document.body.removeChild(el)
 }
 
 export default GradientPage
