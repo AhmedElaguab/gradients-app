d1152a4357a667f3ef0b6ae2550fb03428688e64
AhmedElaguab <elaguabahmed97@gmail.com>
2019-07-05 15:33:04 +0100

    feat: use context api rather than the state

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { useContext } from "react"
 import Gradient from "./Gradient"
+import { GradientsContext } from "./../contexts/GradientsContext"
 
 const PageLayout = props => {
+  const gradients = useContext(GradientsContext)
   return (
     <div className="page-layout-wrapper">
       <div className="container">
         <ul className="page-layout">
-          {props.gradients.map(gradient => (
+          {gradients.map(gradient => (
             <Gradient gradient={gradient} />
           ))}
         </ul>
