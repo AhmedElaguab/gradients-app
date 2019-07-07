b837711fdc7592b6a17d18f8bf380c9aa843b915
AhmedElaguab <elaguabahmed97@gmail.com>
2019-07-07 12:54:50 +0100

    feat: add change direction of gradinet option

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSyncAlt, faCode } from '@fortawesome/free-solid-svg-icons'
 
 const Gradient = props => {
   const [{ name, colors, direction }, setGradient] = useState({
@@ -10,9 +12,44 @@ const Gradient = props => {
     return `linear-gradient(${direction}, ${colors.join(', ')})`
   }
   const gradientValue = getGradientValue(direction, colors)
+
+  // Handle direction click
+  const handleDirectionClick = () => {
+    // Change direction besides to the previous direction
+    const getNextDirection = () => {
+      switch (direction) {
+        case 'to right':
+          return 'to bottom'
+          break
+        case 'to bottom':
+          return 'to left'
+          break
+        case 'to left':
+          return 'to top'
+          break
+        case 'to top':
+          return 'to right'
+          break
+        default:
+          return 'to right'
+          break
+      }
+    }
+    setGradient({ name, colors, direction: getNextDirection() })
+  }
   return (
     <li className="gradient" style={{ background: gradientValue }}>
-      {name} ({colors.length} colors)
+      <p className="gradient__name">
+        {name} ({colors.length} colors)
+      </p>
+      <div className="gradient__options">
+        <FontAwesomeIcon
+          icon={faSyncAlt}
+          title="Rotate the gradient"
+          onClick={handleDirectionClick}
+        />
+        <FontAwesomeIcon icon={faCode} title="Copy the code" />
+      </div>
     </li>
   )
 }
