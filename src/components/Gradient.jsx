c215431a6582b2ba1af81abea32110711d3bf658
AhmedElaguab <elaguabahmed97@gmail.com>
2019-07-07 11:50:42 +0100

    fix: simplify gradient generator

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -4,29 +4,7 @@ const Gradient = props => {
   const { gradient } = props
   // Create gradient function
   const getGradientValue = colors => {
-    switch (colors.length) {
-      case 3:
-        return `linear-gradient(to right, ${colors[0]}, ${colors[1]}, ${
-          colors[2]
-        })`
-        break
-      case 4:
-        return `linear-gradient(to right, ${colors[0]}, ${colors[1]}, 
-          ${colors[2]}, ${colors[3]})`
-        break
-      case 5:
-        return `linear-gradient(to right, ${colors[0]}, ${colors[1]}, 
-          ${colors[2]}, ${colors[3]}, ${colors[4]})`
-        break
-      case 6:
-        return `linear-gradient(to right, ${colors[0]}, ${colors[1]},
-          ${colors[2]}, ${colors[3]}, ${colors[4]}, ${colors[5]})`
-        break
-
-      default:
-        return `linear-gradient(to right, ${colors[0]}, ${colors[1]})`
-        break
-    }
+    return `linear-gradient(to right, ${colors.join(", ")})`
   }
   const gradientValue = getGradientValue(gradient.colors)
   return (
